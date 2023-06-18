20dde3d9762dd005cd178b85c3b80e447c9bfe73
Suyesh Badge <suyeshbadge@gmail.com>
2023-06-18 21:12:26 +0530

    Added request validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  //Request validation settings
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      errorHttpStatusCode: 422,
+    }),
+  );
+  await app.listen(2023);
 }
 bootstrap();
