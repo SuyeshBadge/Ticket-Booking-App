2bff54360fb32746a277cf9571b2bf58530b3251
Suyesh Badge <suyeshbadge@gmail.com>
2023-06-18 19:16:21 +0530

    Connected Database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TicketModule } from './ticket/ticket.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.schema{.ts,.js}'],
+      synchronize: process.env.APP_ENV === 'dev',
+      autoLoadEntities: true,
+    }),
+    TicketModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
